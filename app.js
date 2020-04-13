4a7b81b21a3ec8910d383831b958efe357a93919
leila-io <leilaahmadi.edu@outlook.com>
2020-04-13 18:09:32 +0430

    v1 - Setup new campground POST route; Added in body-parser; Setup route to show form; Added basic unstyled form

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var campgroundsArr = [
+   {
+      name: "Salmon Creek",
+      image: "http://source.unsplash.com/800x400/?creek"
+   },
+   {
+      name: "Granite Hill",
+      image: "http://source.unsplash.com/800x400/?hill"
+   },
+   {
+      name: "Mountain Goat's Rest",
+      image: "http://source.unsplash.com/800x400/?mountain"
+   }
+];
 
 app.get("/", function (req, res) {
    res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-   var campgrounds = [
-      {
-         name: "Salmon Creek",
-         image: "http://source.unsplash.com/800x400/?creek",
-      },
-      {
-         name: "Granite Hill",
-         image: "http://source.unsplash.com/800x400/?hill",
-      },
-      {
-         name: "Mountain Goat's Rest",
-         image: "http://source.unsplash.com/800x400/?mountain",
-      }
-   ]
+   res.render("campgrounds", { campgroundsGallery: campgroundsArr });
+});
 
-    res.render("campgrounds", {campgroundsVar: campgrounds});
+app.post("/campgrounds", function (req, res) {
+   // get data from form and add to campgrounds array
+   var newName = req.body.nameAdded;
+   var newImage = req.body.imageAdded;
+   var newCampground = { name: newName, image: newImage }
+   campgroundsArr.push(newCampground);
+   // redirect to campgrounds page
+   res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+   res.render("new");
 });
 
 app.listen(3000, function () {
