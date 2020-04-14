ff2dcca2a4bb8b96579670a6041b5bb9c3ec9f02
leila-io <leilaahmadi.edu@outlook.com>
2020-04-14 16:56:09 +0430

    v2 - Installed mongoose; Modified the app to use DB

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+   app = express(),
+   bodyParser = require("body-parser"),
+   mongoose = require("mongoose")
 
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var campgroundsArr = [
-   {
-      name: "Salmon Creek",
-      image: "http://source.unsplash.com/800x400/?creek"
-   },
-   {
-      name: "Granite Hill",
-      image: "http://source.unsplash.com/800x400/?hill"
-   },
-   {
-      name: "Mountain Goat's Rest",
-      image: "http://source.unsplash.com/800x400/?mountain"
-   },
-   {
-      name: "Himalayas",
-      image: "http://source.unsplash.com/800x400/?himalaya"
-   },
-   {
-      name: "Washington, D.C.",
-      image: "http://source.unsplash.com/800x400/?washington"
-   },
-   {
-      name: "New York City",
-      image: "http://source.unsplash.com/800x400/?new+york"
-   }
-];
+// CAMPGROUNDS SCHEMA
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//    {
+//       name: "Tehran",
+//       image: "http://source.unsplash.com/800x400/?tehran"
+//    }, function (err, campground) {
+//       if (err) {
+//          console.log(err);
+//       } else {
+//          console.log("THE NEWLY CREATED CAMPGROUND IS:")
+//          console.log(campground);
+//       }
+//    }
+// )
 
 app.get("/", function (req, res) {
    res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-   res.render("campgrounds", { campgroundsGallery: campgroundsArr });
+   // get all campgrounds from DB
+   Campground.find({}, function (err, allCampgrounds) {
+      if (err) {
+         console.log(err);
+      } else {
+         res.render("campgrounds", { campgroundsGallery: allCampgrounds });
+      }
+   })
+
 });
 
 app.post("/campgrounds", function (req, res) {
@@ -45,9 +51,15 @@ app.post("/campgrounds", function (req, res) {
    var newName = req.body.nameAdded;
    var newImage = req.body.imageAdded;
    var newCampground = { name: newName, image: newImage }
-   campgroundsArr.push(newCampground);
-   // redirect to campgrounds page
-   res.redirect("/campgrounds");
+   // create a new campground and save to DB
+   Campground.create(newCampground, function (err, newlyCreated) {
+      if (err) {
+         console.log(err);
+      } else {
+         // redirect to campgrounds page
+         res.redirect("/campgrounds");
+      }
+   });
 });
 
 app.get("/campgrounds/new", function (req, res) {
