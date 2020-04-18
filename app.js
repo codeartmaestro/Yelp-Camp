72d272e0f355608292cc65a5622a0668e1283810
leila-io <leilaahmadi.edu@outlook.com>
2020-04-18 08:45:26 +0430

    basic styling

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ seedDB();
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
