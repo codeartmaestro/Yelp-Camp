52f0f86e5c5403058a4f2c9c9e2ed434c0523fc7
leila-io <leilaahmadi.edu@outlook.com>
2020-04-21 09:23:51 +0430

    v10 - Campgrounds authorization, edit, update, destroy

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
+var Comment = require("../models/comment");
 // ===========================================================
 
 // INDEX - show all campgrounds
@@ -25,8 +26,8 @@ router.post("/", isLoggedIn, function (req, res) {
    // below is another of doing the same thing we did for author in comments
    var author = {
       id: req.user._id,
-      username: req.user.username
-   }
+      username: req.user.username,
+   };
    var newCampground = { name: newName, image: newImage, description: newDescription, author: author };
    // create a new campground and save to DB
    Campground.create(newCampground, function (err, newlyCreated) {
@@ -59,12 +60,73 @@ router.get("/:id", function (req, res) {
       });
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", checkCampgraoundOwnership, function (req, res) {
+   Campground.findById(req.params.id, function (err, foundCampground) {
+      res.render("campgrounds/edit", { campground: foundCampground });
+   });
+});
+
+// UPDTAE CAMPGROUND ROUTE
+router.put("/:id", checkCampgraoundOwnership, function (req, res) {
+   // find and update the correct campground
+   Campground.findByIdAndUpdate(req.params.id, req.body.edittedCampground, function (err, updtaedCampground) {
+      if (err) {
+         console.log(err);
+         res.redirect("/campgrounds");
+      } else {
+         res.redirect("/campgrounds/" + req.params.id);
+      }
+   });
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", checkCampgraoundOwnership, function (req, res) {
+   Campground.findByIdAndRemove(req.params.id, function (err, campgroundRemoved) {
+      if (err) {
+         res.redirect("/campgrounds/" + req.params.id);
+      } else {
+         Comment.deleteMany({ _id: { $in: campgroundRemoved.comments } }, function (err) {
+            if (err) {
+               console.log(err);
+            }
+            res.redirect("/campgrounds");
+         });
+      }
+   });
+});
+
 // Middleware to check if the user is logged in
 function isLoggedIn(req, res, next) {
    if (req.isAuthenticated()) {
       return next();
    }
    res.redirect("/login");
+}
+
+// Middleware for authorization
+function checkCampgraoundOwnership(req, res, next) {
+   // Is the user logged in? If yes...
+   if (req.isAuthenticated()) {
+      Campground.findById(req.params.id, function (err, foundCampground) {
+         if (err) {
+            // "back" sends the user back to where they were before this page
+            res.redirect("back");
+         } else {
+            // Does the user own this campground?
+            // We need to check if the ID of the author matches the ID of the user
+            if (foundCampground.author.id.equals(req.user._id)) {
+               next();
+            } else {
+               // if the user does not own the campground
+               res.redirect("back");
+            }
+         }
+      });
+   } else {
+      // If not logged in, redirect
+      res.redirect("back");
+   }
 }
 
 module.exports = router;
