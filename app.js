c995a90de0c431ac47f3fa0affd913ca0bb3029b
leila-io <leilaahmadi.edu@outlook.com>
2020-04-23 11:11:21 +0430

    revise port

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 // port
-app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
    console.log("The YelpCamp Server has started!");
 });
